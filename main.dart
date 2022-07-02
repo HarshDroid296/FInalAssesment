4e8ed9cdd7b4c0429d2d6227a379635ee943d7e7
Harsh Sharma <73656003+HarshDroid296@users.noreply.github.com>
2022-07-02 16:07:16 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter_application_1_1/9_Menu.dart';
+import 'package:flutter_application_1_1/Screen_1.dart';
+import 'package:flutter_application_1_1/Screen_2.dart';
+//import 'ButtonsDemo.dart';
+//import 'package:flutter_application_1_1/MyApp.dart';
+
+void main(){
+  //runApp(MyApp());
+  //runApp(ButtonsDemo());
+  //runApp(Test());
+  //runApp(MenuDemo());
+  runApp(Screen1());
+}
